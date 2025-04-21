b9db0e3b5a59b3b2ba1bfd39a026b1193322adb8
Kazi Mridul <kazimridulwebdev@gmail.com>
2025-04-21 15:48:30 +0600

    Full Project Done

--- js/script.js
+++ js/script.js
@@ -5,3 +5,32 @@ $('.counter').counterUp({
     delay: 10,
     time: 3000
 });
+// slick slider
+$('.slider-for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.slider-nav',
+    speed:2000,
+   
+  });
+  $('.slider-nav').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    asNavFor: '.slider-for',
+    dots: false,
+    centerMode: true,
+    focusOnSelect: true,
+    arrows:false
+  });
+
+//   review slider
+$('.mySlider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    prevArrow:'<i class="fa-solid arrows prr fa-arrow-left"></i>',
+    nextArrow:'<i class="fa-solid arrows nrr fa-arrow-right"></i>',
+  });
+      
