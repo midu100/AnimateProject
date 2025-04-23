66185112ff1135f674ba8a0f2dc0b4cf52841178
Kazi Mridul <kazimridulwebdev@gmail.com>
2025-04-23 20:57:02 +0600

    Responsive Done

--- js/script.js
+++ js/script.js
@@ -13,6 +13,49 @@ $('.slider-for').slick({
     fade: true,
     asNavFor: '.slider-nav',
     speed:2000,
+    responsive: [
+      {
+        breakpoint: 575,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false,
+          arrows:true,
+          prevArrow:'<i class="fa-solid resArrow resP fa-arrow-left"></i>',
+          nextArrow:'<i class="fa-solid resArrow resR fa-arrow-right"></i>',
+        }
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false,
+          arrows:true,
+          prevArrow:'<i class="fa-solid resArrow resP fa-arrow-left"></i>',
+          nextArrow:'<i class="fa-solid resArrow resR fa-arrow-right"></i>',
+        }
+      },
+      {
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false,
+          arrows:true,
+          prevArrow:'<i class="fa-solid resArrow resP fa-arrow-left"></i>',
+          nextArrow:'<i class="fa-solid resArrow resR fa-arrow-right"></i>',
+        }
+      },
+
+
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
    
   });
   $('.slider-nav').slick({
@@ -26,11 +69,71 @@ $('.slider-for').slick({
   });
 
 //   review slider
-$('.mySlider').slick({
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    prevArrow:'<i class="fa-solid arrows prr fa-arrow-left"></i>',
-    nextArrow:'<i class="fa-solid arrows nrr fa-arrow-right"></i>',
-  });
+
+// $('.mySlider').slick({
+//     infinite: true,
+//     slidesToShow: 3,
+//     slidesToScroll: 1,
+//     prevArrow:'<i class="fa-solid arrows prr fa-arrow-left"></i>',
+//     nextArrow:'<i class="fa-solid arrows nrr fa-arrow-right"></i>',
+//     responsive: [
+//       {
+//         breakpoint: 575,
+//         settings: {
+//           slidesToShow: 1,
+//           slidesToScroll: 1,
+//           infinite: true,
+//           dots: false,
+//         }
+//       },
+
+
+
+// c
+
+
+//       // You can unslick at a given breakpoint now by adding:
+//       // settings: "unslick"
+//       // instead of a settings object
+//     ]
+//   });
       
+$('.mySlider').slick({
+  lazyLoad: 'ondemand',
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  prevArrow:'<i class="fa-solid arrows prr fa-arrow-left"></i>',
+  nextArrow:'<i class="fa-solid arrows nrr fa-arrow-right"></i>',
+  responsive: [
+           {
+             breakpoint: 575,
+             settings: {
+               slidesToShow: 1,
+               slidesToScroll: 1,
+               infinite: true,
+               dots: false,
+             }
+           },
+           {
+            breakpoint: 767,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              infinite: true,
+              dots: false,
+            }
+          },
+          {
+            breakpoint: 991,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              infinite: true,
+              dots: false,
+            }
+          },
+
+
+          ]
+
+});
